b5077da57606a55e0484867850d280c1a530450f
Sergey Zaitsev <saw.ai@ya.ru>
2020-11-12 00:49:49 +0300

    postgresql

--- repeater.py
+++ repeater.py
@@ -1,12 +1,11 @@
 # check if I already working with newest version
 
-
 import os
 my_pid = str(os.getpid())
 
 from subprocess import check_output
 try:
-    pids = check_output("ps aux | grep 'python3 repeater.py' | grep -v grep | grep -v 'cd /root/' | awk '{print $2}'", shell=True).decode().split()
+    pids = check_output("ps aux | grep 'python3 repeater.py' | grep -v grep | grep -v 'cd /var/' | awk '{print $2}'", shell=True).decode().split()
 except:
     pids = None
 
@@ -41,16 +40,16 @@ import threading
 from collections import defaultdict
 
 from storage import Storage
-
+storage = Storage()
 
 
 bot = telebot.TeleBot('1307955102:AAFjWxksNaeQNpna8kGMcRucCxwLz2GfYDE')
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-  def options():
-    bot.send_message(message.chat.id, "/word - Добавить слово\n/test - Угадай слово\n/freq - Test frequent words\n/list - Список слов\n/delete - Удалить слово")
-  options()
+    def options():
+        bot.send_message(message.chat.id, "/word - Добавить слово\n/test - Угадай слово\n/freq - Test frequent words\n/list - Список слов\n/delete - Удалить слово")
+    options()
 
 
 dd = defaultdict(dict)
@@ -110,7 +109,7 @@ def how_many(x):
         s = "слова"
     else:
         s = "слов"
-    
+
     return "{} {}".format(x, s)
 
 
@@ -118,143 +117,141 @@ def how_many(x):
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-  d = dd[message.chat.id]
+    d = dd[message.chat.id]
 
-  storage = Storage('storage.db')
+    if 'mode' not in d:
+        d['mode'] = 'waiting for the answer'
 
-  if 'mode' not in d:
-      d['mode'] = 'waiting for the answer'
-
-  print (f'thread_id={threading.get_ident()}')
+    print (f'thread_id={threading.get_ident()}')
 
 
-  def options():
-    bot.send_message(message.chat.id, "/freq - Test frequent words\n/list - List of unknown words") #\n/test - Угадай слово\n/list - Список слов\n/delete - Удалить слово\n/word - Добавить слово\n")
-  def send(text):
-    bot.send_message(message.chat.id, text)
+    def options():
+        bot.send_message(message.chat.id, "/freq - Test frequent words\n/list - List of unknown words") #\n/test - Угадай слово\n/list - Список слов\n/delete - Удалить слово\n/word - Добавить слово\n")
+    def send(text):
+        bot.send_message(message.chat.id, text)
 
-  try:
-    if False and message.text == 'delete_all_words':
-        v[0] = {'_all' : 0, '_correct' : 0} 
+    try:
+        if False and message.text == 'delete_all_words':
+            v[0] = {'_all' : 0, '_correct' : 0}
 
 
-    elif False and message.text == '/test':
-        if vsize() < 4:
-            bot.send_message(message.chat.id, "Для теста нужно хотя бы 4 слова. Сейчас у тебя только {}. Набери /word, чтобы добавить слово".format(vsize()))
-            d['mode'] = 'waiting for a word'
-        else:
-            markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
-            cache = json.load(open('cache.json'))
-            if 'words' in cache:
-                words = cache['words']
+        elif False and message.text == '/test':
+            if vsize() < 4:
+                bot.send_message(message.chat.id, "Для теста нужно хотя бы 4 слова. Сейчас у тебя только {}. Набери /word, чтобы добавить слово".format(vsize()))
+                d['mode'] = 'waiting for a word'
             else:
-                words = random.sample([x for x in v[0] if not x.startswith('_')], 4)
-                with open('cache.json', 'w') as f:
-                    json.dump({'words' : words}, f)
+                markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+                cache = json.load(open('cache.json'))
+                if 'words' in cache:
+                    words = cache['words']
+                else:
+                    words = random.sample([x for x in v[0] if not x.startswith('_')], 4)
+                    with open('cache.json', 'w') as f:
+                        json.dump({'words' : words}, f)
 
-            word = words[0]
-            d['question'] = word
-            random.shuffle(words)
+                word = words[0]
+                d['question'] = word
+                random.shuffle(words)
 
-            buttons = [types.KeyboardButton(v[0][w]) for w in words]
-            for button in buttons:
-                markup.add(button)
-            bot.send_message(message.chat.id, word + "?", reply_markup = markup)
+                buttons = [types.KeyboardButton(v[0][w]) for w in words]
+                for button in buttons:
+                    markup.add(button)
+                bot.send_message(message.chat.id, word + "?", reply_markup = markup)
+                d['mode'] = 'waiting for the answer'
+                d['correct'] = v[0][word]
+
+        elif 'candidates' not in d or message.text == '/freq':
+            markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+            words = storage.get_words(message.chat.id, 4, 500) + ['ALRIGHT']
+            for word in words:
+                markup.add(word)
+            bot.send_message(message.chat.id, 'asking', reply_markup = markup)
+
             d['mode'] = 'waiting for the answer'
-            d['correct'] = v[0][word]
-
-    elif 'candidates' not in d or message.text == '/freq':
-        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
-        words = storage.get_words(message.chat.id, 4, 500) + ['ALRIGHT']
-        for word in words:
-            markup.add(word)
-        bot.send_message(message.chat.id, 'asking', reply_markup = markup)
-
-        d['mode'] = 'waiting for the answer'
-        d['candidates'] = words[:4]
-        d['markup'] = markup
+            d['candidates'] = words[:4]
+            d['markup'] = markup
 
 
-    elif message.text == '/photo':
-        create_picture()
-        img = open('picture.png', 'rb')
-        bot.send_chat_action(message.chat.id, 'upload_photo')
-        bot.send_photo(message.chat.id, img)
-        img.close()
+        elif message.text == '/photo':
+            create_picture()
+            img = open('picture.png', 'rb')
+            bot.send_chat_action(message.chat.id, 'upload_photo')
+            bot.send_photo(message.chat.id, img)
+            img.close()
 
-    elif False and message.text == '/word':
-        d['mode'] = 'waiting for a word'
-        bot.send_message(message.chat.id, "Новое слово")
-
-    elif message.text == '/list':
-
-        words = storage.get_words(message.chat.id, 30, top=30, label=2)
-        bot.send_message(message.chat.id, '\n'.join(words))
-
-    elif False and message.text == '/delete':
-        d['mode'] = 'waiting for deletion'
-        bot.send_message(message.chat.id, "Какое слово удалить?")
-        
-    elif False and d['mode'] == 'waiting for deletion':
-        if message.text not in v[0]:
-            bot.send_message(message.chat.id, "Нет такого слова, еще разок")
-        else:
-            del v[0][message.text]
-            send("Удалено! Теперь у тебя {}".format(how_many(vsize())))
+        elif False and message.text == '/word':
             d['mode'] = 'waiting for a word'
-            options()
+            bot.send_message(message.chat.id, "Новое слово")
 
-    elif False and d['mode'] == 'waiting for a word':
-        if not ( 'a' <= message.text[0] and message.text[0] <= 'z'):
-            send("English word, please!")
-        else:
-            d['word'] = message.text
-            send("Теперь перевод")
-            d['mode'] = 'waiting for translation'
+        elif message.text == '/list':
 
-    elif False and d['mode'] == 'waiting for translation':
-        v[0][d['word']] = message.text
+            words = storage.get_words(message.chat.id, 30, top=30, label=2)
+            bot.send_message(message.chat.id, '\n'.join(words))
+
+        elif False and message.text == '/delete':
+            d['mode'] = 'waiting for deletion'
+            bot.send_message(message.chat.id, "Какое слово удалить?")
+
+        elif False and d['mode'] == 'waiting for deletion':
+            if message.text not in v[0]:
+                bot.send_message(message.chat.id, "Нет такого слова, еще разок")
+            else:
+                del v[0][message.text]
+                send("Удалено! Теперь у тебя {}".format(how_many(vsize())))
+                d['mode'] = 'waiting for a word'
+                options()
+
+        elif False and d['mode'] == 'waiting for a word':
+            if not ( 'a' <= message.text[0] and message.text[0] <= 'z'):
+                send("English word, please!")
+            else:
+                d['word'] = message.text
+                send("Теперь перевод")
+                d['mode'] = 'waiting for translation'
+
+        elif False and d['mode'] == 'waiting for translation':
+            v[0][d['word']] = message.text
+            dump()
+            send("Теперь у тебя {}".format(how_many(vsize())))
+            d['mode'] = 'waiting for a word'
+
+        elif d['mode'] == 'waiting for the answer':
+
+
+            if message.text == 'ALRIGHT':
+                storage.change_status(message.chat.id, d['candidates'], 1)
+            else:
+                storage.change_status(message.chat.id, [message.text], 2)
+                send(f'The word "{message.text}" is marked as unknown')
+
+            d['mode'] = 'waiting for a word'
+
+            amount = storage.get_count(message.chat.id, None)
+            known = storage.get_count(message.chat.id, 1)
+            unknown = storage.get_count(message.chat.id, 2)
+            bot.send_message(message.chat.id, f'all: {amount}, known={known}, unknown={unknown}', reply_markup = d['markup'])
+
+            markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+            words = storage.get_words(message.chat.id, 4, 500) + ['ALRIGHT']
+
+            for word in words:
+                markup.add(word)
+            bot.send_message(message.chat.id, 'asking', reply_markup = markup)
+
+            d['mode'] = 'waiting for the answer'
+            d['candidates'] = words[:4]
+            d['markup'] = markup
+
         dump()
-        send("Теперь у тебя {}".format(how_many(vsize())))
-        d['mode'] = 'waiting for a word'
 
-    elif d['mode'] == 'waiting for the answer':
-
-
-        if message.text == 'ALRIGHT':
-            storage.change_status(message.chat.id, d['candidates'], 1)
-        else:
-            storage.change_status(message.chat.id, [message.text], 2)
-            send(f'The word "{message.text}" is marked as unknown')
-
-        d['mode'] = 'waiting for a word'
-
-        amount = storage.get_count(message.chat.id, None)
-        known = storage.get_count(message.chat.id, 1)
-        unknown = storage.get_count(message.chat.id, 2)
-        bot.send_message(message.chat.id, f'all: {amount}, known={known}, unknown={unknown}', reply_markup = d['markup'])
-
-        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
-        words = storage.get_words(message.chat.id, 4, 500) + ['ALRIGHT']
-
-        for word in words:
-            markup.add(word)
-        bot.send_message(message.chat.id, 'asking', reply_markup = markup)
-
-        d['mode'] = 'waiting for the answer'
-        d['candidates'] = words[:4]
-        d['markup'] = markup
-
-    dump()
-
-    print("message.text = ", message.text)
-    print(v[0])
-    print(d)
-  except KeyboardInterrupt:
-      raise
-  except:
-      raise
-      send("Ошибочка вышла...")
+        print("message.text = ", message.text)
+        print(v[0])
+        print(d)
+    except KeyboardInterrupt:
+        raise
+    except:
+        raise
+        send("Ошибочка вышла...")
 
 
 @bot.message_handler(commands=['switch'])
--- storage.py
+++ storage.py
@@ -1,66 +1,67 @@
 import sqlite3
 from pandas import DataFrame
 import pandas as pd
+import psycopg2
 
 class Storage:
-    def __init__(self, DB_FILE):
-        conn = sqlite3.connect(DB_FILE)
-        cursor = conn.cursor()
-        cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='WORDS';")
-        if not cursor.fetchall():
-            cursor.execute('CREATE TABLE WORDS (word text, frequency number)')
-            conn.commit()
-            df = pd.read_csv('unigram_freq.csv').set_index('word')
-            df.to_sql('WORDS', conn, if_exists='replace')
+    def __init__(self):
+        self.conn = psycopg2.connect(dbname='repeater', user='saw_ai', password='12345', host='localhost')
+        self.cursor = self.conn.cursor()
 
+        #cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='WORDS';")
+        #if not cursor.fetchall():
+        #    cursor.execute('CREATE TABLE WORDS (word text, frequency number)')
+        #    conn.commit()
+        #    df = pd.read_csv('unigram_freq.csv').set_index('word')
+        #    df.to_sql('WORDS', conn, if_exists='replace')
 
-        cursor.execute("PRAGMA table_info(WORDS);")
-        self.users = set(list(map(lambda x : x[1], cursor.fetchall()))[2:])
-        self.conn = conn
+        self.cursor.execute("SELECT column_name FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'words';")
+        self.users = set(list(map(lambda x : x[0][1:], self.cursor.fetchall()))[3:])
+
+        self.cursor.execute('SELECT word from words')
+        self.words = set(map(lambda x : x[0].strip(), self.cursor.fetchall()))
+
 
     def add_user(self, user_id):
         if str(user_id) not in self.users:
-            cursor = self.conn.cursor()
-            cursor.execute(f"ALTER TABLE WORDS ADD COLUMN `{user_id}` NUMBER default 0;")
+            self.cursor.execute(f"ALTER TABLE WORDS ADD COLUMN _{user_id} NUMBER default 0;")
             self.conn.commit()
             self.users.add(user_id)
 
     def get_words(self, user_id, count, top=500, label=0):
         self.add_user(user_id)
-        cursor = self.conn.cursor()
-        cursor.execute(f"SELECT * from (SELECT word FROM WORDS WHERE `{user_id}`={label} LIMIT {top}) ORDER BY RANDOM() LIMIT {count};")
-        return list(map(lambda x : x[0], cursor.fetchall()))
+        self.cursor.execute(f"SELECT * from (SELECT word FROM WORDS WHERE _{user_id}={label} ORDER BY index LIMIT {top}) AS x ORDER BY RANDOM() LIMIT {count};")
+        return list(map(lambda x : x[0].strip(), self.cursor.fetchall()))
 
 
     def change_status(self, user_id, words, label):
         self.add_user(user_id)
-        cursor = self.conn.cursor()
+        self.add_words(user_id, set(words) - self.words)
         words_list = "'" + "', '".join(words) + "'"
         query = f"""
             UPDATE words 
-            SET `{user_id}` = {label}
+            SET _{user_id} = {label}
             WHERE word in ({words_list});"""
-        cursor.execute(query)
+        self.cursor.execute(query)
         self.conn.commit()
 
     def get_values(self, user_id):
         self.add_user(user_id)
-        cursor = self.conn.cursor()
-        cursor.execute(f"SELECT `{user_id}` FROM words;")
-        return list(map(lambda x : x[0], cursor.fetchall()))
+        self.cursor.execute(f"SELECT _{user_id} FROM words;")
+        return list(map(lambda x : x[0], self.cursor.fetchall()))
 
     def get_count(self, user_id, label=None):
         self.add_user(user_id)
-        cursor = self.conn.cursor()
         if label is None:
-            cursor.execute(f"SELECT count(*) FROM words;")
+            self.cursor.execute(f"SELECT count(*) FROM words;")
         else:
-            cursor.execute(f"SELECT count(*) FROM words WHERE `{user_id}`={label};")
-        return cursor.fetchall()[0][0]
+            self.cursor.execute(f"SELECT count(*) FROM words WHERE _{user_id}={label};")
+        return self.cursor.fetchall()[0][0]
 
+    def add_words(self, user_id, words):
+        if words:
+            for word in words:
+                self.cursor.execute(f"INSERT INTO words (word) VALUES ('{word}')")
+                self.words.add(word)
+            self.conn.commit()
 
-#storage.change_status(user, ['of', 'and'], 1)
-#print (storage.get_words(user, 5))
-#storage = Storage()
-#print(storage.users)
-#print (Storage('storage.db').get_words('id12345', 30, top=30, label=1))
